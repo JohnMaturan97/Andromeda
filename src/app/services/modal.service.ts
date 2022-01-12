0d1c07d4570eba930f4a8ac18475ce69ed5a8960
JohnMaturan97 <borntocode1997@gmail.com>
2022-01-12 12:19:50 -0500

    files missing | PR issue

---
+++ src/app/services/modal.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+interface IModal {
+  id: string;
+  visible : boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ModalService {
+  private modals = []
+
+  constructor() { }
+
+  isModalOpen() {
+    return this.visible
+  }
+
+  toggleModal() {
+    this.visible = !this.visible;
+  }
+}
